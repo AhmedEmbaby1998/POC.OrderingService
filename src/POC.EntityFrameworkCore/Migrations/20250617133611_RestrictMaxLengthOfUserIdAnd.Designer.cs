b841b505af96ab6da9eb1c37c2201972277e0862
Ahmed Embaby <ahmed.m.embaby@gizasystems.com>
2025-06-17 16:38:51 +0300

    EF core migration of CorrelationId and UserId

--- src/POC.EntityFrameworkCore/Migrations/20250617133611_RestrictMaxLengthOfUserIdAnd.Designer.cs
+++ src/POC.EntityFrameworkCore/Migrations/20250617133611_RestrictMaxLengthOfUserIdAnd.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using POC.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace POC.Migrations
 {
     [DbContext(typeof(POCDbContext))]
-    partial class POCDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250617133611_RestrictMaxLengthOfUserIdAnd")]
+    partial class RestrictMaxLengthOfUserIdAnd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
